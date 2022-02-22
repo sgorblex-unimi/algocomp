6c0f219893c5d52229b925387dfb484c2a4251c1
Edoardo Marangoni <ecmm@anche.no>
2022-02-22 21:39:44 +0100

    usa lualatex

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 # Build two times to generate indices, references, citations and all that jazz. 
-pdflatex -shell-escape -file-line-error main.tex && bibtex main && pdflatex -shell-escape -file-line-error main.tex && pdflatex -shell-escape -file-line-error main.tex
+lualatex -shell-escape -file-line-error main.tex && bibtex main && lualatex -shell-escape -file-line-error main.tex && lualatex -shell-escape -file-line-error main.tex
 rm -r main.aux main.bbl main.blg main.fdb_latexmk main.fls main.log main.out main.synctex.gz main.toc _minted-main

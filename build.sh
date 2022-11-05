3ee876f259e15ed134707162af2300569a7db5c9
Alessandro Clerici <alessandro.clericilorenzini@studenti.unimi.it>
2022-11-05 19:52:47 +0100

    refactoring e debloat generale

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-# Build two times to generate indices, references, citations and all that jazz. 
-lualatex -shell-escape -file-line-error main.tex && bibtex main && lualatex -shell-escape -file-line-error main.tex && lualatex -shell-escape -file-line-error main.tex
-rm -r main.aux main.bbl main.blg main.fdb_latexmk main.fls main.log main.out main.synctex.gz main.toc main.lof main.lot _minted-main 
